9dd81703edf0364230d3806dfd8d46932e357b4d
ZhangBY619 <boyangzhang246@gmail.com>
2026-04-02 23:00:13 +0800

    添加了新的图片，用于移动端软件

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'xin.zbyblq.done',
+  appName: 'whativedone',
+  webDir: 'dist'
+};
+
+export default config;
